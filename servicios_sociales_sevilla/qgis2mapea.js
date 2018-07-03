029c519ca26974807437e9364a509404e7a4b694
Patricio Soriano <pasoriano@gmail.com>
2018-07-03 10:37:45 +0200

    proxy true

--- servicios_sociales_sevilla/qgis2mapea.js
+++ servicios_sociales_sevilla/qgis2mapea.js
@@ -1,14 +1,15 @@
+var proxy = false
 var layerName = 'Servicios Sociales (Datos Abiertos Sevilla)'
 var fillColor = '#3dc439'
 var strokeColor = 'blue'
 var bb = [233470.824576,4135243.407779,240069.982919,4145294.270417]
 var basemaps = ['cdau', 'cdau_satelite', 'cdau_hibrido']
 
-//M.proxy(false);
+M.proxy(proxy);
 
 mapajs = M.map({
     container: "map",
-    controls:['layerswitcher', 'scale', 'mouse', 'Scaleline', 'panzoombar', 'location'],
+    controls:['layerswitcher', 'layerswitcher', 'scale', 'scale', 'mouse', 'mouse', 'Scaleline', 'Scaleline', 'panzoombar', 'panzoombar', 'location', 'location'],
     // center: {
       // x: 350373,
       // y: 4190372
@@ -18,7 +19,6 @@ mapajs = M.map({
     wmcfiles: basemaps
   });
 
-//M.proxy(false);
 
 mapajs.setProjection ("EPSG:25830*d");
 
@@ -50,5 +50,4 @@ var layerQGIS2Mapea4 = new M.layer.GeoJSON(
 layerQGIS2Mapea4.setStyle(estiloLayer);
 mapajs.addLayers(layerQGIS2Mapea4);
 
-//M.proxy(false);
 
