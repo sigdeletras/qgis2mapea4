0825f05eb5f33c518e3ef1f39d2a0b19ff936a10
Patricio Soriano <pasoriano@gmail.com>
2018-07-03 11:36:59 +0200

    Traducciones proxy

--- i18n/qgis2mapea4_es.ts
+++ i18n/qgis2mapea4_es.ts
@@ -6,137 +6,130 @@
     <message>
         <location filename="../qgis2mapea.py" line="211"/>
         <source>&amp;QGIS to Mapea4</source>
-        <translatorcomment>QGIS a Mapea4</translatorcomment>
         <translation>&amp;QGIS a Mapea4</translation>
     </message>
     <message>
         <location filename="../qgis2mapea.py" line="198"/>
         <source>QGIS to Mapea4</source>
-        <translatorcomment>QGIS a Mapea4</translatorcomment>
         <translation>QGIS a Mapea4</translation>
     </message>
 </context>
 <context>
     <name>QGIS2Mapea4DialogBase</name>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="14"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="15"/>
         <source>QGIS to Mapea4</source>
-        <translatorcomment>QGIS a Mapea4 :: SIGdeletras</translatorcomment>
         <translation>QGIS a Mapea4 :: SIGdeletras</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="26"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="27"/>
         <source>Viewer title</source>
-        <translatorcomment>Nombre de la capa</translatorcomment>
         <translation>Nombre de la capa</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="42"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="88"/>
         <source>Select a vector layer</source>
-        <translatorcomment>Seleccione capa vectorial</translatorcomment>
         <translation>Seleccione capa vectorial</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="61"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="107"/>
         <source>Only selected features</source>
-        <translatorcomment>Solo elementos seleccionados</translatorcomment>
         <translation>Solo elementos seleccionados</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="76"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="122"/>
         <source>Download folder</source>
-        <translatorcomment>Ubicación de archivos</translatorcomment>
         <translation>Ubicación de archivos</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="101"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="147"/>
         <source>...</source>
-        <translatorcomment>...</translatorcomment>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="116"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="162"/>
         <source>Base maps (WMC)</source>
-        <translatorcomment>Mapas base (WMC)</translatorcomment>
         <translation>Mapas base (WMC)</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="125"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="178"/>
         <source>CDAU</source>
-        <translatorcomment>CDAU</translatorcomment>
         <translation>CDAU</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="132"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="171"/>
         <source>Orthoimage</source>
-        <translatorcomment>Ortoimágen</translatorcomment>
         <translation>Ortoimágen</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="139"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="185"/>
         <source>Hybrid</source>
-        <translatorcomment>Híbrida</translatorcomment>
         <translation>Híbrida</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="154"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="200"/>
         <source>Viewer options</source>
-        <translatorcomment>Opciones del visor</translatorcomment>
         <translation>Opciones del visor</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="163"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="230"/>
         <source>ScaleLine</source>
-        <translatorcomment>Escala gráfica</translatorcomment>
         <translation>Escala gráfica</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="170"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="209"/>
         <source>Mouse</source>
-        <translatorcomment>Coordenadas</translatorcomment>
         <translation>Coordenadas</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="177"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="223"/>
         <source>Layer switcher</source>
-        <translatorcomment>Árbol de capas</translatorcomment>
         <translation>Árbol de capas</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="184"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="244"/>
         <source>Location</source>
-        <translatorcomment>Posición</translatorcomment>
         <translation>Posición</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="191"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="237"/>
         <source>Panzoom Bar</source>
-        <translatorcomment>Barra de desplazamiento</translatorcomment>
         <translation>Barra de desplazamiento</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="198"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="216"/>
         <source>Scale</source>
-        <translatorcomment>Escala numérica</translatorcomment>
         <translation>Escala numérica</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="213"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="259"/>
         <source>Plugins</source>
-        <translatorcomment>Complementos</translatorcomment>
         <translation>Complementos</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="222"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="271"/>
         <source>Search street</source>
-        <translatorcomment>Buscador de vías</translatorcomment>
         <translation>Buscador de vías</translation>
     </message>
     <message>
-        <location filename="../qgis2mapea_dialog_base.ui" line="268"/>
+        <location filename="../qgis2mapea_dialog_base.ui" line="327"/>
         <source>Ok</source>
-        <translatorcomment>Crear</translatorcomment>
         <translation>Crear</translation>
+    </message>
+    <message>
+        <location filename="../qgis2mapea_dialog_base.ui" line="43"/>
+        <source>Proxy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qgis2mapea_dialog_base.ui" line="62"/>
+        <source>Off (local)</source>
+        <translation>Desactivado (archivos locales)</translation>
+    </message>
+    <message>
+        <location filename="../qgis2mapea_dialog_base.ui" line="72"/>
+        <source>On (server)</source>
+        <translation>Activado (archivos servidor)</translation>
     </message>
 </context>
 </TS>
